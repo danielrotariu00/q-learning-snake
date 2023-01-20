d983121db1b560586c2d58754dbc1d8eb48bc545
Daniel Rotariu <daniel.rotariu00@gmail.com>
2023-01-20 14:32:25 +0200

    Uploaded Project

---
+++ Snake/Snake/MainWindow.xaml.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Snake
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly Dictionary<GridValue, ImageSource> gridValToImage = new()
+        {
+            { GridValue.Empty, Images.Empty },
+            { GridValue.Snake, Images.Body },
+            { GridValue.Food, Images.Food }
+        };
+
+        private readonly Dictionary<Direction, int> dirToRotation = new()
+        {
+            { Direction.Up, 0 },
+            { Direction.Right, 90 },
+            { Direction.Down, 180 },
+            { Direction.Left, 270 }
+        };
+
+        private Image[,] gridImages;
+        private readonly Random random = new Random();
+        private GameState gameState;
+        private QLearning qLearning = new QLearning();
+        private bool isRunning;
+        private bool isTraining = false;
+
+        private int rows = 20, cols = 20;
+        private int numberOfTrainingGames = 500;
+        private int trainingSpeedDelay = 2;
+        private int testingSpeedDelay = 50;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void TrainButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!isRunning)
+            {
+                isRunning = true;
+                isTraining = true;
+
+                Setup();
+                InitializeQLearning();
+                InitializeGameState();
+
+                await RunTraining();
+                isRunning = false;
+                isTraining = false;
+            }
+        }
+
+        private async void TestButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!isRunning)
+            {
+                isRunning = true;
+
+                Setup();
+                InitializeGameState();
+
+                await RunTesting();
+                isRunning = false;
+            }
+        }
+
+        private async Task RunTraining()
+        {
+            int currentGame = 1;
+
+            Draw();
+            Overlay.Visibility = Visibility.Hidden;
+
+            while (currentGame <= numberOfTrainingGames)
+            {
+                GamesCountText.Text = $"TRAINING AGENT - Game: {currentGame}/{numberOfTrainingGames}";
+                await TrainingLoop();
+                await DrawDeadSnake();
+                await Task.Delay(trainingSpeedDelay);
+                currentGame++;
+                InitializeGameState();
+            }
+
+            ShowTrainingOver();
+        }
+
+        private async Task TrainingLoop()
+        {
+            while (!gameState.GameOver)
+            {
+                await Task.Delay(trainingSpeedDelay);
+                MoveAndDraw();
+            }
+        }
+
+        private async Task RunTesting()
+        {
+            Draw();
+            Overlay.Visibility = Visibility.Hidden;
+            await TestingLoop();
+            await DrawDeadSnake();
+            ShowTestingOver();
+        }
+
+        private async Task TestingLoop()
+        {
+            while (!gameState.GameOver)
+            {
+                GamesCountText.Text = $"TESTING AGENT - Score: {gameState.Score}";
+                await Task.Delay(testingSpeedDelay);
+                MoveAndDraw();
+            }
+        }
+
+        private void MoveAndDraw()
+        {
+            Direction nextDirection = qLearning.GetNextMove(gameState, isTraining);
+            gameState.ChangeDirection(nextDirection);
+            int oldScore = gameState.Score;
+            gameState.Move();
+            if (gameState.Score > oldScore)
+            {
+                AddFood();
+            }
+            Draw();
+        }
+
+        private Image[,] SetupGrid()
+        {
+            Image[,] images = new Image[rows, cols];
+            GameGrid.Rows = rows;
+            GameGrid.Columns = cols;
+            GameGrid.Width = GameGrid.Height * (cols / (double)rows);
+            GameGrid.Children.Clear();
+
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < cols; c++)
+                {
+                    Image image = new Image()
+                    {
+                        Source = Images.Empty,
+                        RenderTransformOrigin = new Point(0.5, 0.5)
+                    };
+
+                    images[r, c] = image;
+                    GameGrid.Children.Add(image);
+                }
+            }
+
+            return images;
+        }
+
+        private void AddFood()
+        {
+            List<Position> empty = new List<Position>(gameState.EmptyPositions());
+
+            if (empty.Count == 0)
+            {
+                return;
+            }
+
+            Position foodPosition = empty[random.Next(empty.Count)];
+            gameState.AddFood(foodPosition);
+        }
+
+        private void Draw()
+        {
+            DrawGrid();
+            DrawSnakeHead();
+        }
+
+        private void DrawGrid()
+        {
+            for (int r = 0; r < rows; r ++)
+            {
+                for (int c = 0; c < cols; c++)
+                {
+                    GridValue gridVal = gameState.Grid[r, c];
+                    gridImages[r, c].Source = gridValToImage[gridVal];
+                    gridImages[r, c].RenderTransform = Transform.Identity;
+                }
+            }
+        }
+
+        private void DrawSnakeHead()
+        {
+            Position headPos = gameState.HeadPosition();
+            Image image = gridImages[headPos.Row, headPos.Col];
+            image.Source = Images.Head;
+
+            int rotation = dirToRotation[gameState.Dir];
+            image.RenderTransform = new RotateTransform(rotation);
+        }
+
+        private async Task DrawDeadSnake()
+        {
+            List<Position> positions = new List<Position>(gameState.SnakePositions());
+
+            for (int i = 0; i < positions.Count; i++)
+            {
+                Position pos = positions[i];
+                ImageSource source = (i == 0) ? Images.DeadHead : Images.DeadBody;
+                gridImages[pos.Row, pos.Col].Source = source;
+
+                if (isTraining)
+                {
+                    await Task.Delay(trainingSpeedDelay);
+                }
+                else
+                {
+                    await Task.Delay(testingSpeedDelay);
+                }
+                
+            }
+        }
+
+        private void Setup()
+        {
+            numberOfTrainingGames = Int32.Parse(NumberOfTrainingGamesTextBox.Text);
+            rows = Int32.Parse(RowsTextBox.Text);
+            cols = Int32.Parse(ColumnsTextBox.Text);
+            trainingSpeedDelay = Int32.Parse(TrainingSpeedDelayTextBox.Text);
+            testingSpeedDelay = Int32.Parse(TestingSpeedDelayTextBox.Text);
+
+            gridImages = SetupGrid();
+        }
+
+        private void InitializeQLearning()
+        {
+            double epsilon = Double.Parse(EpsilonTextBox.Text);
+            double learningRate = Double.Parse(LearningRateTextBox.Text);
+            double discountFactor = Double.Parse(DiscountFactorTextBox.Text);
+
+            qLearning = new QLearning(epsilon, learningRate, discountFactor);
+        }
+
+        private void InitializeGameState()
+        {
+            gameState = new GameState(rows, cols);
+            AddFood();
+        }
+
+        private void ShowTrainingOver()
+        {
+            Overlay.Visibility = Visibility.Visible;
+            OverlayText.Text = "Training done. Press the Test button to test the agent.";
+        }
+
+        private void ShowTestingOver()
+        {
+            Overlay.Visibility = Visibility.Visible;
+            OverlayText.Text = "Testing done.";
+        }
+    }
+}
---
+++ Snake/Snake/QLearning.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Snake
+{
+    public class QLearning
+    {
+        private Dictionary<String, double[]> qTable = new Dictionary<String, double[]>();
+
+        private readonly Random random = new Random();
+        private double epsilon = 0.1;
+        private double learningRate = 0.1;
+        private double discountFactor = 0.1;
+
+        private int GAME_OVER_REWARD = -1000;
+        private int EAT_FOOD_REWARD = 10;
+        private int MOVE_CLOSER_TO_FOOD_REWARD = 1;
+        private int MOVE_AWAY_FROM_FOOD_REWARD = -1;
+
+        public QLearning()
+        {
+            InitializeQTable();
+        }
+
+        public QLearning(double epsilon, double learningRate, double discountFactor)
+        {
+            InitializeQTable();
+
+            this.epsilon = epsilon;
+            this.learningRate = learningRate;
+            this.discountFactor = discountFactor;
+        }
+
+        public Direction GetNextMove(GameState gameState, bool isTraining)
+        {
+            int action;
+            double prob = random.NextDouble();
+            String currentState = GetState(gameState);
+
+            if (isTraining && prob <= epsilon)
+            {
+                action = GetRandomAction();
+            } 
+            else
+            {
+                action = GetBestAction(currentState);
+            }
+
+            Direction direction = GetActionDirection(gameState, action);
+            GameState nextGameState = gameState.DeepClone();
+
+            nextGameState.ChangeDirection(direction);
+            nextGameState.Move();
+
+            if (isTraining)
+            {
+                UpdateQTable(gameState, action, nextGameState);
+            }
+
+            return direction;
+        }
+
+        private String GetState(GameState gameState)
+        {
+            Direction headDirection = gameState.Dir;
+            Direction foodDirection = gameState.Dir;
+            bool dangerLeft;
+            bool dangerRight;
+            bool dangerUp;
+            bool dangerDown;
+
+            Position headPosition = gameState.HeadPosition();
+            Position foodPosition = gameState.foodPosition;
+
+
+            if (headPosition.Col > foodPosition.Col)
+            {
+                foodDirection = Direction.Left;
+
+                if (headPosition.Row > foodPosition.Row)
+                {
+                    foodDirection = Direction.LeftUp;
+                }
+                else if (headPosition.Row < foodPosition.Row)
+                {
+                    foodDirection = Direction.LeftDown;
+                }
+            }
+            else if (headPosition.Col < foodPosition.Col)
+            {
+                foodDirection = Direction.Right;
+
+                if (headPosition.Row > foodPosition.Row)
+                {
+                    foodDirection = Direction.RightUp;
+                }
+                else if (headPosition.Row < foodPosition.Row)
+                {
+                    foodDirection = Direction.RightDown;
+                }
+            } 
+            else
+            {
+                if (headPosition.Row > foodPosition.Row)
+                {
+                    foodDirection = Direction.Up;
+                }
+                else if (headPosition.Row < foodPosition.Row)
+                {
+                    foodDirection = Direction.Down;
+                }
+            }
+
+            dangerLeft = IsDanger(gameState, headPosition, Direction.Left);
+            dangerRight = IsDanger(gameState, headPosition, Direction.Right);
+            dangerUp = IsDanger(gameState, headPosition, Direction.Up);
+            dangerDown = IsDanger(gameState, headPosition, Direction.Down);
+
+            return $"{headDirection.GetHashCode()},{foodDirection.GetHashCode()},{dangerLeft},{dangerRight},{dangerUp},{dangerDown}";
+        }
+
+        private int GetRandomAction()
+        {
+            return random.Next(0, 3);
+        }
+
+        private int GetBestAction(String state)
+        {
+            double[] actionsValues = qTable.GetValueOrDefault(state);
+
+            int bestAction = 0;
+            for(int i = 1; i < actionsValues.Length; i++)
+            {
+                if (actionsValues[i] > actionsValues[bestAction])
+                {
+                    bestAction = i;
+                }
+            }
+
+            return bestAction;
+        }
+
+        private Direction GetActionDirection(GameState gameState, int action)
+        {
+            Direction headDirection = gameState.Dir;
+
+            switch (action)
+            {
+                case 1:
+                    if (headDirection == Direction.Up || headDirection == Direction.Down)
+                    {
+                        return Direction.Left;
+                    }
+                    else
+                    {
+                        return Direction.Up;
+                    }
+                case 2:
+                    if (headDirection == Direction.Up || headDirection == Direction.Down)
+                    {
+                        return Direction.Right;
+                    }
+                    else
+                    {
+                        return Direction.Down;
+                    }
+                default:
+                    return headDirection;
+            }
+        }
+
+        private bool IsDanger(GameState gameState, Position headPosition, Direction direction)
+        {
+            Position newHeadPos = headPosition.Translate(direction);
+            GridValue hit = gameState.WillHit(newHeadPos);
+
+            if (hit == GridValue.Outside || hit == GridValue.Snake)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private void UpdateQTable(GameState gameState, int action, GameState nextGameState)
+        {
+            String oldState = GetState(gameState);
+            String newState = GetState(nextGameState);
+            int reward;
+
+            if (nextGameState.GameOver)
+            {
+                reward = GAME_OVER_REWARD;
+
+                // Bellman Equation - No Future State
+                qTable[oldState][action] = qTable[oldState][action] + learningRate * (reward - qTable[oldState][action]);
+            }
+            else
+            {
+                if (nextGameState.HeadPosition().Equals(gameState.foodPosition))
+                {
+                    reward = EAT_FOOD_REWARD;
+                }
+                else if (nextGameState.RowDistanceFromFood() < gameState.RowDistanceFromFood() ||
+                    nextGameState.ColDistanceFromFood() < gameState.ColDistanceFromFood())
+                {
+                    reward = MOVE_CLOSER_TO_FOOD_REWARD;
+                }
+                else
+                {
+                    reward = MOVE_AWAY_FROM_FOOD_REWARD;
+                }
+
+                // Bellman Equation
+                int nextStateBestAction = GetBestAction(newState);
+                qTable[oldState][action] = qTable[oldState][action] + learningRate * 
+                    (reward + discountFactor * qTable[newState][nextStateBestAction] - qTable[oldState][action]);
+            }
+        }
+
+        private void InitializeQTable()
+        {
+            // State = (Snake head direction, Food direction relative to snake head, DangerLeft, DangerRight, DangerUp, DangerDown)
+            // Action = [No action, Valid action 1, Valid action 2]
+            //      snake head direction = left => valid actions are up and down
+            //      snake head direction = right => valid actions are up and down
+            //      snake head direction = up => valid actions are left and right
+            //      snake head direction = down => valid actions are left and right
+
+            Direction[] headDirections = new Direction[4]
+            {
+                Direction.Left,
+                Direction.Right,
+                Direction.Up,
+                Direction.Down,
+            };
+
+            Direction[] foodDirections = new Direction[8]
+            {
+                Direction.Left,
+                Direction.Right,
+                Direction.Up,
+                Direction.Down,
+                Direction.LeftUp,
+                Direction.LeftDown,
+                Direction.RightUp,
+                Direction.RightDown
+            };
+
+            bool[] dangerLeftOptions = new bool[2]
+            {
+                false,
+                true
+            };
+
+            bool[] dangerRightOptions = new bool[2]
+            {
+                false,
+                true
+            };
+
+            bool[] dangerUpOptions = new bool[2]
+            {
+                false,
+                true
+            };
+
+            bool[] dangerDownOptions = new bool[2]
+            {
+                false,
+                true
+            };
+
+            foreach (Direction headDirection in headDirections)
+            {
+                foreach (Direction foodDirection in foodDirections)
+                {
+                    foreach (bool dangerLeft in dangerLeftOptions)
+                    {
+                        foreach (bool dangerRight in dangerRightOptions)
+                        {
+                            foreach (bool dangerUp in dangerUpOptions)
+                            {
+                                foreach (bool dangerDown in dangerDownOptions)
+                                {
+                                    String key = $"{headDirection.GetHashCode()},{foodDirection.GetHashCode()},{dangerLeft},{dangerRight},{dangerUp},{dangerDown}";
+                                    qTable.Add(key, new double[3] { 0, 0, 0 });
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
